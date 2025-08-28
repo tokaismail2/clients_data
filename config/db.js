40a7dd90cac5cf4917a73282454b1bcc83a6fc96
tokaismail11 <Toka.Ismail@cgtech.com.sa>
2025-08-28 17:58:50 +0300

    new connection

--- config/db.js
+++ config/db.js
@@ -2,11 +2,19 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log(" MongoDB Connected");
+    if (!process.env.MONGO_URI) {
+      throw new Error("MONGO_URI is not defined in environment variables");
+    }
+
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("MongoDB Connected");
   } catch (err) {
     console.error("MongoDB Connection Failed:", err.message);
-    process.exit(1);
+
   }
 };
 
